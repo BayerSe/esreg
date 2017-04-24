c9e945c179a4cf7b3515a4b50203d69d5be76d88
Sebastian Bayer <BayerSe@gmail.com>
2017-04-24 17:44:24 +0200

    remove help file

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,12 +1,3 @@
-
-#' Verify the derivatives of the specification functions
-#'
-#' Can be used to check whether the derivatives of G1 and G2_curly are properly implemented.
-#'
-#' @param g1 type of the g1 function
-#' @param g2 type of the g2 function
-#' @param z value at which we evalaut the derivatives. Random if NULL.
-#' @param tol Tolerance of the derivative
 .verify_g_function <- function(g1, g2, z = NULL, tol = 1e-08) {
     h <- 1e-05
     if (is.null(z))
